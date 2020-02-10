2590de438b0db1dae57117861ac890d1aefb02d3
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-10 19:58:58 +0530

    ADDED:function for user deletion

--- address_book.scala
+++ address_book.scala
@@ -1,3 +1,5 @@
+
+
 /*
 Filename: address_book
 
@@ -20,48 +22,90 @@ all open address books and terminating the program.
 */
 
 
-object address_book{
+object address_book {
   def main(args: Array[String]): Unit = {
     var flag = true
-    while(flag){
-      try{
-        var p:Array[Person] = new Array[Person](4)
-        for(i<-p.indices){
-          p(i) = new Person()
-        }
+    while (flag) {
+      try {
 
-        for(i <-p.indices){
-          p(i).addInfo()
-        }
-
-        for(i <-p.indices){
-          p(i).display()
-        }
-
-        p = sort_By_Lastname(p)
-
-        for(i <-p.indices){
-          p(i).display()
+        print("How much People do you want to add in Address book : ")
+        var people = scala.io.StdIn.readInt()
+        var personData: Array[Person] = new Array[Person](people)
+        for (i <- personData.indices) {
+          personData(i) = new Person()
+          personData(i).addInfo()
+          delete_info(personData)
         }
 
         flag = false
       }
-      catch{
-        case _=>print("Something went wrong Error occurred.")
+      catch {
+        case _ => print("Something went wrong Error occurred.")
       }
     }
   }
-  class Person{
-    var first_name:String = _
-    var last_name:String = _
-    var address:String = _
-    var city:String =  _
-    var state:String =  _
-    var zip:Long = _
-    var phone:Long = _
+
+  def sort_By_Lastname(people: Array[Person]): Array[Person] = {
+    for (i <- 0 until people.length - 1) {
+      for (j <- 0 until people.length - 1 - i) {
+        if (people(j).last_name.compareTo(people(j + 1).last_name) > 0) {
+          var temp = people(j)
+          people(j) = people(j + 1)
+          people(j + 1) = temp
+        }
+      }
+    }
+    people
+  }
+  def delete_info(people: Array[Person]): Array[Person] = {
+     var operation_flag:Boolean = true
+    while(operation_flag) {
+      print("Enter user's first name to delete its record : ")
+      var name = scala.io.StdIn.readLine()
+      var presnt_flag: Boolean = false
+      var index = 0
+      for (i <- people.indices) {
+        if (people(i).first_name.equals(name)) {
+          print("First name is present."+index)
+          presnt_flag = true
+          operation_flag = false
+
+        }
+
+        if(!presnt_flag){
+          index += 1
+        }
+
+      }
+      if(!presnt_flag){
+        print("User not fount do you still want to delete (y) : ")
+        var input = scala.io.StdIn.readLine()
+        if(!input.equals("y")){
+
+          operation_flag = false
+        }
+      }
+    }
+    people
+  }
+
+  def delete_user(people: Array[Person],name:String): Array[Person] = {
+
+
+  }
+
+
+  class Person {
+    var first_name: String = _
+    var last_name: String = _
+    var address: String = _
+    var city: String = _
+    var state: String = _
+    var zip: Long = _
+    var phone: Long = _
 
     //Function to add data in contacts
-    def addInfo(): Unit ={
+    def addInfo(): Unit = {
       println("Enter the first name : ")
       this.first_name = scala.io.StdIn.readLine()
 
@@ -75,56 +119,108 @@ object address_book{
       this.city = scala.io.StdIn.readLine()
 
       var inputflag = true
-      while(inputflag){
-        try{
+      while (inputflag) {
+        try {
           println("Enter Your zip code : ")
           this.zip = scala.io.StdIn.readLong()
-          if(zip >= 100000 && zip <= 999999 ){
+          if (zip >= 100000 && zip <= 999999) {
             inputflag = false
           }
-        }catch {
-          case _=>{
+        } catch {
+          case _ => {
             print("Please enter valid input : \n")
           }
         }
       }
       inputflag = true
-      while(inputflag){
-        try{
+      while (inputflag) {
+        try {
           println("Enter Your Phone number : ")
           this.phone = scala.io.StdIn.readLong()
-          inputflag = false
-        }catch {
-          case _=>{
+          if (phone.toString.length == 10) {
+            inputflag = false
+          }
+        } catch {
+          case _ => {
             print("Please enter valid input : \n")
           }
         }
       }
     }
+
     //Function to display data
-    def display(): Unit ={
-      var data:String = ""
-      data += "First name : "+this.first_name +"\n"
-      data += "Last name : "+this.last_name +"\n"
-      data += "Address : "+this.address +"\n"
-      data += "City : "+this.city +"\n"
-      data += "Zip code : "+this.zip +"\n"
-      data += "Phone number : "+this.phone +"\n"
+    def display(): Unit = {
+      var data: String = ""
+      data += "First name : " + this.first_name + "\n"
+      data += "Last name : " + this.last_name + "\n"
+      data += "Address : " + this.address + "\n"
+      data += "City : " + this.city + "\n"
+      data += "Zip code : " + this.zip + "\n"
+      data += "Phone number : " + this.phone + "\n"
       print(data)
     }
-  }
 
-  def sort_By_Lastname(people:Array[Person]):Array[Person]={
-    for(i <- 0 until people.length-1){
-      for(j <- 0 until people.length-1-i){
-        if(people(j).last_name.compareTo(people(j+1).last_name) > 0){
-          var temp = people(j)
-          people(j) = people(j+1)
-          people(j+1) = temp
+
+    def editInfo(): Unit = {
+      var choice_flag = true
+      while (choice_flag) {
+        print("\nWhat do you want to edit ? :\n" +
+          "1.Address\n2.City\n3.Zip code\n4.Phone number")
+        var choice = scala.io.StdIn.readInt()
+        if (choice == 1) {
+          println("Enter your Address : ")
+          this.address = scala.io.StdIn.readLine()
+
+
+        } else if (choice == 2) {
+          println("Enter your City : ")
+          this.city = scala.io.StdIn.readLine()
+
+        } else if (choice == 3) {
+          var inputflag = true
+          while (inputflag) {
+            try {
+              println("Enter Your zip code : ")
+              this.zip = scala.io.StdIn.readLong()
+              if (zip >= 100000 && zip <= 999999) {
+                inputflag = false
+              }
+            } catch {
+              case _ => {
+                print("Please enter valid input : \n")
+              }
+            }
+          }
+
+
+        } else if (choice == 4) {
+
+          var inputflag = true
+          while (inputflag) {
+            try {
+              println("Enter Your Phone number : ")
+              this.phone = scala.io.StdIn.readLong()
+              if (phone.toString.length == 10) {
+                inputflag = false
+              }
+            } catch {
+              case _ => {
+                print("Please enter valid input : \n")
+              }
+            }
+          }
+        } else {
+          print("please enter valid choice : \n")
         }
+
+        print("Do you want more edits (n) :")
+        var ans: String = scala.io.StdIn.readLine()
+        if (ans.equals("n")) {
+          choice_flag = false
+        }
+
       }
     }
-  people
   }
 
 } 
