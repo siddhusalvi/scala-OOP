400dc0423a283ea7758d32fe5ea209355d8cfbe9
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-10 12:36:46 +0530

    REFACTORED: Symbol emoji

--- DeckOfCards.scala
+++ DeckOfCards.scala
@@ -16,11 +16,12 @@ object DeckOfCards{
     while(flag){
       try{
         var cat:Cards = new Cards()
+        print("In how many players do you want to distribute the cards : ")
+        var players = scala.io.StdIn.readInt()
         //cat.shuffle()
-        //cat.display()
+        cat.distribute(players)
         flag = false
-        var dt = Array(1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52)
-        cat.view(dt)
+
       }
 
       catch{
@@ -29,28 +30,30 @@ object DeckOfCards{
     }
   }
   class Cards{
-    var cat:Array[Int] = new Array[Int](52)
+    var total_cards = 52
+    var current_card = 0
+    var pack:Array[Int] = new Array[Int](52)
     for(i <- 0 until 52){
-      cat(i) = i
+      pack(i) = i
     }
 
     def shuffle(): Unit ={
-      for(i <- 0 until cat.length){
-        var rnd:Int = Random.nextInt(cat.length-1)
-        var temp:Int =  cat(rnd)
-        cat(rnd) = cat(i)
-        cat(i) = temp
+      for(i <- 0 until pack.length){
+        var rnd:Int = Random.nextInt(pack.length-1)
+        var temp:Int =  pack(rnd)
+        pack(rnd) = pack(i)
+        pack(i) = temp
       }
     }
 
     def display(): Unit ={
-      println(cat.mkString(" "))
+      println(pack.mkString(" "))
     }
 
-    def view(dt:Array[Int]):Unit={
+    def view(givenData:Array[Int]):Unit={
 
-      for(i <- 0 until cat.length){
-        var normal_card = cat(i) //remove cat add dt and rename dt
+      for(i <- 0 until givenData.length){
+        var normal_card = givenData(i) //remove cat add dt and rename dt
 
         if(normal_card <= 12){
           var card_type = "Club"
@@ -61,7 +64,7 @@ object DeckOfCards{
           }else if(type_card == 10){
             card_str += "Queen"
           }else if(type_card == 11){
-            card_str += "King"
+            card_str += "♚"
           }else if(type_card == 12) {
             card_str += "Ace"
           }else{
@@ -79,7 +82,7 @@ object DeckOfCards{
           }else if(type_card == 10){
             card_str += "Queen"
           }else if(type_card == 11){
-            card_str += "King"
+            card_str += "♚"
           }else if(type_card == 12) {
             card_str += "Ace"
           }else{
@@ -96,7 +99,7 @@ object DeckOfCards{
           }else if(type_card == 10){
             card_str += "Queen"
           }else if(type_card == 11){
-            card_str += "King"
+            card_str += "♚"
           }else if(type_card == 12) {
             card_str += "Ace"
           }else{
@@ -113,21 +116,126 @@ object DeckOfCards{
           }else if(type_card == 10){
             card_str += "Queen"
           }else if(type_card == 11){
-            card_str += "King"
+            card_str += "♚"
           }else if(type_card == 12) {
             card_str += "Ace"
           }else{
             card_str += (type_card+2).toString
           }
           println(card_str)
-
-
         }else{
           println("card is out of index ")
         }
       }
     }
 
+    def printCard(given_card:Int): Unit ={
+      if(given_card <= 12){
+        var card_type = "♣"
+        var card_str = card_type + " "
+        var type_card = given_card%13
+        if( type_card == 9){
+          card_str += "Jack"
+        }else if(type_card == 10){
+          card_str += "Queen"
+        }else if(type_card == 11){
+          card_str += "♚"
+        }else if(type_card == 12) {
+          card_str += "Ace"
+        }else{
+          card_str += (type_card+2).toString
+        }
+        print(card_str)
+
+      }else if (given_card <= 25){
+
+        var card_type = "♦ "
+        var card_str = card_type + " "
+        var type_card = given_card%13
+        if( type_card == 9){
+          card_str += "Jack"
+        }else if(type_card == 10){
+          card_str += "Queen"
+        }else if(type_card == 11){
+          card_str += "♚"
+        }else if(type_card == 12) {
+          card_str += "Ace"
+        }else{
+          card_str += (type_card+2).toString
+        }
+        print(card_str)
+
+      }else if (given_card <= 38){
+        var card_type = "♥ "
+        var card_str = card_type + " "
+        var type_card = given_card%13
+        if( type_card == 9){
+          card_str += "Jack"
+        }else if(type_card == 10){
+          card_str += "Queen"
+        }else if(type_card == 11){
+          card_str += "♚"
+        }else if(type_card == 12) {
+          card_str += "Ace"
+        }else{
+          card_str += (type_card+2).toString
+        }
+        print(card_str)
+
+      }else if(given_card <=51){
+        var card_type = "♠ "
+        var card_str = card_type + " "
+        var type_card = given_card%13
+        if( type_card == 9){
+          card_str += "Jack"
+        }else if(type_card == 10){
+          card_str += "Queen"
+        }else if(type_card == 11){
+          card_str += "♚"
+        }else if(type_card == 12) {
+          card_str += "Ace"
+        }else{
+          card_str += (type_card+2).toString
+        }
+        print(card_str)
+      }else{
+        print("card is out of index ")
+      }
+
+
+
+    }
+
+    def get_card(): Int ={
+      var card = pack(current_card)
+      current_card += 1
+      card
+    }
+    def distribute(players:Int): Unit ={
+      if(players < 0 || players > 52){
+        println("Invalid input Not possible to distribute : ")
+      }else{
+        var card_set = total_cards / players
+        println("Each player will get "+card_set+" Cards")
+        var distributed_cards = Array.ofDim[String](players , card_set)
+        for(i <- 0 until players){
+          for(j <- 0 until card_set){
+            distributed_cards(i)(j) = get_card().toString
+          }
+        }
+
+        for(i <- 0 until players){
+          print("player "+(i+1)+" : ")
+          for(j <- 0 until card_set){
+            printCard(distributed_cards(i)(j).toInt)
+            print(", ")
+          }
+          println()
+        }
+
+
+      }
+    }
 
 
   }
