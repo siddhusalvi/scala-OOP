0757feab2b766bce8f69e2b5570b423f44de4255
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-13 17:41:46 +0530

    ADDED:functions to load data save data and display report

--- inventory_data_management.scala
+++ inventory_data_management.scala
@@ -1,17 +1,8 @@
-import java.lang
+import java.io.FileWriter
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.gson.Gson
 
-import scala.util.parsing.json.{JSON, Parser}
 import scala.io.Source
-import play.api.libs.json._
-import net.liftweb.json._
-import net.liftweb.json.DefaultFormats
-
-import scala.collection.mutable._
-import scala.util.parsing.combinator.JavaTokenParsers
-
 
 /*
 Filename: inventory_data_management
@@ -30,79 +21,118 @@ e. O/P -> Create the JSON from Inventory Object and output the JSON String
 
 
 object inventory_data_management {
-  case class Rice(name:String, quantity:Int, price: Int){
-    def getString(): String ={
-      var strData = name +" "+ quantity.toString +" "+price.toString
-      strData
-    }
-
-  }
-
-  case class Wheat(name:String, quantity:Int, price: Int){
-    def getString(): String ={
-      var strData = name +" "+ quantity.toString +" "+price.toString
-      strData
-    }
-  }
-
-  case class Pulse(name:String, quantity:Int, price: Int){
-    def getString(): String ={
-      var strData = name +" "+ quantity.toString +" "+price.toString
-      strData
-    }
-  }
-
-
-
-
 
   def main(args: Array[String]): Unit = {
+    var flag = true
+    var load_flag = false
+    var add_flag = false
+    var json_mgr = new Gson()
+    var grains = loadData()
+    while (flag) {
 
+      print("1:load data from JSON \t2:Create new data \t3:Calculate total report \t4:Save data \t5:exit : ")
+      var choice = scala.io.StdIn.readInt()
+      if (choice == 5) {
+        flag = false
+      } else if (choice == 1) {
+        grains = loadData()
+        print("Data loaded from JSON : ")
+        println("\nRice : ")
+        for (i <- grains) {
+          println("\t" + i.getString)
+        }
+        load_flag = true
+      } else if (choice == 2) {
+        grains = createData()
+        add_flag = true
+      } else if (choice == 3) {
+        if(load_flag || add_flag){
+          calculateReport(grains)
+        } else{
+          print("Please add data to inventory to create report \n")
+        }
 
+      } else if (choice == 4) {
+        if(load_flag || add_flag){
+          saveData(grains)
+          println("Data saved in JSON file")
+        } else{
+          print("Please add data to inventory to store it in local file \n")
+        }
 
-    var pulese:Array[Pulse] = new Array[Pulse](3)
-
-    pulese(0) = new Pulse("basmati",100,50)
-    pulese(1) = new Pulse("wadaKolam",100,30)
-    pulese(2) = new Pulse("biryani",80,40)
-
-    var wheat:Array[Wheat] = new Array[Wheat](3)
-
-    wheat(0) = new Wheat("basmati",100,50)
-    wheat(1) = new Wheat("wadaKolam",100,30)
-    wheat(2) = new Wheat("biryani",80,40)
-
-
-    var rice:Array[Rice] = new Array[Rice](3)
-
-    rice(0) = new Rice("basmati",100,50)
-    rice(1) = new Rice("wadaKolam",100,30)
-    rice(2) = new Rice("biryani",80,40)
-
-
-
-
-
-
-    var gson = new Gson()
-
-    var jsonStr = gson.toJson(pulese)
-    println(jsonStr)
-
-    var temp = gson.fromJson(jsonStr,classOf[Array[Pulse]])
-
-    for(i <- temp.indices){
-      println(temp(i).getString())
+      }else {
+        print("Please enter valid input : ")
+      }
     }
+  }
+  def loadData():Array[Inventory]={
+    var json_mgr = new Gson()
+    var grain_data: String = "/home/admin1/IdeaProjects/OOPs/src/main/scala/JsonData/grains.json"
+    var strGrains = getFileData(grain_data)
+    var data:Array[Inventory] = json_mgr.fromJson(strGrains, classOf[Array[Inventory]])
+    data
+  }
+  //Function to create new report
+  def createData(): Array[Inventory] = {
+    print("How many whole grains do you want to add ")
+    var grain_count = scala.io.StdIn.readInt()
+    var data: Array[Inventory] = new Array[Inventory](grain_count)
+    for (i <- data.indices) {
+      println("Enter the grain : ")
+      var grain = scala.io.StdIn.readLine()
+      println("Enter the name of grain ")
+      var name = scala.io.StdIn.readLine()
+      println("Enter the quantity : ")
+      var quantity = scala.io.StdIn.readInt()
+      println("Enter the price : ")
+      var price = scala.io.StdIn.readInt()
+      data(i) = new Inventory(grain, name, quantity, price)
+      println(data(i).getString)
+    }
+    return data
+  }
+  //Function to calculate report
+  def calculateReport(data:Array[Inventory]): Unit ={
+    var report_str:String = ""
+    for(i <-0 until data.length){
+      report_str += "Grain : "+ data(i).grain.toString + " Name : " + data(i).name.toString + " Quantity : " + data(i).quantity.toString + " Price: " + data(i).price.toString + " Total Money : " + (data(i).quantity * data(i).price).toString + "\n"
+    }
+    println(report_str)
+  }
+  //Function to save data in json
+  def saveData(data: Array[Inventory]):Unit ={
 
-
-
+    var json_mgr = new Gson()
+    var grain_data: String = "/home/admin1/IdeaProjects/OOPs/src/main/scala/JsonData/grains.json"
+    var grain_file = new FileWriter(grain_data)
+    var json_str = json_mgr.toJson(data)
+    grain_file.write(json_str)
+    println("Following data will be stored in file : "+json_str)
+    grain_file.close()
 
   }
 
+  def getFileData(path: String): String = {
+    var file = path
+    var data = Source.fromFile(file)
+    var sentence: String = ""
+    for (line <- data.getLines) {
+      sentence += line
+    }
+    sentence
+  }
 
 
+  //=================================================================================================================
 
+
+  //Rice class to store rice data
+  case class Inventory(grain: String, name: String, quantity: Int, price: Int) {
+    def getString: String = {
+      var strData = this.grain + " " + this.name + " " + this.quantity.toString + " " + this.price.toString
+      strData
+    }
+  }
 
 
 }
