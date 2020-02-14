24741b333622305047179c62070818551ba44093
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-14 10:21:12 +0530

    ADDED:stock manager program

--- stock_account.scala
+++ stock_account.scala
@@ -1,5 +1,4 @@
 import scala.io.Source
-
 /*
 Filename: stock_account
 Created: Siddhesh Salvi
@@ -19,34 +18,34 @@ object stock_account {
   def main(args: Array[String]): Unit = {
     var flag = true
     while (flag) {
-      //      try{
-      var path = "/home/admin1/IdeaProjects/OOPs/src/main/scala/JsonData/stocks"
-      var file_data = getFileData(path)
-      var data_arr = file_data.split(" ")
-      if (data_arr.length % 3 != 0) {
-        println("Error occured while opening stock file")
-      }
-      var stock_count = data_arr.length / 3
-      var stocks = new Array[Stock](stock_count)
+      try {
+        var path = "/home/admin1/IdeaProjects/OOPs/src/main/scala/JsonData/stocks"
+        var file_data = getFileData(path)
+        var data_arr = file_data.split(" ")
+        if (data_arr.length % 3 != 0) {
+          println("Error occurred while opening stock file")
+        }
+        var stock_count = data_arr.length / 3
+        var stocks = new Array[Stock](stock_count)
 
-      for (index <- 0 until stocks.length) {
-        var obj_index = index * 3
-        var stock_name = data_arr(obj_index)
-        var stock_count = data_arr(obj_index + 1).toInt
-        var stock_price = data_arr(obj_index + 2).toInt
-        stocks(index) = new Stock(stock_name, stock_count, stock_price)
-        println("Stock is : "+stocks(index).getString)
+        for (index <- 0 until stocks.length) {
+          var obj_index = index * 3
+          var stock_name = data_arr(obj_index)
+          var stock_count = data_arr(obj_index + 1).toInt
+          var stock_price = data_arr(obj_index + 2).toInt
+          stocks(index) = new Stock(stock_name, stock_count, stock_price)
+          println("Stock is : " + stocks(index).getString)
+        }
+        calculate_report(stocks)
+        flag = false
       }
-      calculate_report(stocks)
-
-      flag = false
-      //      }
-      //      catch{
-      //        case _=>print("Something went wrong Error occurred.")
-      //      }
+      catch {
+        case _ => print("Something went wrong Error occurred.")
+      }
     }
   }
 
+  //Function to calculate stock report
   def calculate_report(data: Array[Stock]): Unit = {
     var total = 0
     println()
@@ -54,7 +53,7 @@ object stock_account {
     for (i <- 0 until data.length) {
       var amount = data(i).price * data(i).count
       total += amount
-      println(data(i).name +" :  "+data(i).price+" * "+data(i).count+" = amount " + amount)
+      println(data(i).name + " :  " + data(i).price + " * " + data(i).count + " = amount " + amount)
     }
     println("Total Price of all shares : " + total)
 
@@ -71,11 +70,13 @@ object stock_account {
     sentence
   }
 
+  //Stock class to store stock data
   class Stock {
     var name = ""
     var count = 0
     var price = 0
 
+    //Constructor
     def this(name: String, count: Int, price: Int) {
       this()
       this.name = name
@@ -83,6 +84,7 @@ object stock_account {
       this.price = price
     }
 
+    //Function to sent String
     def getString(): String = {
       this.name + " " + this.count.toString + " " + this.price.toString
     }
